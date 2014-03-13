6a08ec12bf7f0fd0de570c60f489fa073348e0f9
Erik Sandberg <erik@repairtechsolutions.com>
2014-03-12 22:43:50 -0700

    Created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarpoolOrganizer::Application.config.secret_key_base = '68bf0bfa62e80f76f6548e22271564e9f7149398cb404295ff5afb1d16b892f6dde5f854c01c77e3066b4ec58853d682cf3263bfa2fa93ad28860a8100c45655'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CarpoolOrganizer::Application.config.session_store :cookie_store, key: '_carpool-organizer_session'
